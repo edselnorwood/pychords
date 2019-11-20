a02d9b4fa1f05356f33f9907381ee7983200a6e6
edselnorwood <edselnorwood83@gmail.com>
2019-11-20 14:06:25 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pychords',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Edsel Norwood, Tony Guy, Walter Asbell, Ishaun',
+      author_email='pychords_hack19@gmail.com',
+      license='MIT',
+      packages=['pychords'],
+      zip_safe=False)
